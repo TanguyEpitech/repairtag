7bf0d62696184e052ca3f631c090e54ea0fd1cfa
tanguy <tlegalldutertre@gmail.com>
2022-03-25 14:39:35 +0100

    dklj

---
+++ backend.php
@@ -0,0 +1,35 @@
+<?php
+
+
+
+class form {
+
+    private $input1;
+    private $input2;
+    private $input3;
+
+
+        public function __construct($data)
+        {
+              
+            for ($i=0; $i < count($data) ; $i++) { 
+    
+                # code...
+            }
+    
+    
+        }
+    
+    }
+    
+    
+    print_r($_POST);
+    
+    $user = new form($_POST)
+    
+
+
+?>
+
+
+
