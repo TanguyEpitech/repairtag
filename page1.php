a56680ec3758cb9f79dc01923eb7621dc1c12b17
tanguy <tlegalldutertre@gmail.com>
2022-03-26 14:40:48 +0100

    lkjklj

---
+++ page1.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+    <html>
+
+    <head>
+        <meta charset="utf-8">
+        <title>Page 1</title>
+        
+        <link rel="stylesheet" href="/page1.css">
+    </head>
+
+    <body>
+        <div data-layer="ce32396c-a119-4f72-9350-d30dee464e55" class="page1">        <div data-layer="46d348bc-0869-4a22-a8c9-c364ec2cef41" class="rectangle28"></div>
+        <div data-layer="655bc3c2-895a-49c1-a9e3-8b7aec3ff5c8" class="infosReparabilite">Infos Réparabilité</div>
+        <div data-layer="69218497-e0b4-4464-97f9-d2dd228d9ad8" class="rectangle7"></div>
+        <div data-layer="2f508c93-b25b-4eb8-87d6-09b760562255" class="rectangle8"></div>
+        <div data-layer="039e5702-638e-40b0-8a8b-ac38f7fede3b" class="diagnosticReparation">Diagnostic Réparation</div>
+        <div data-layer="840e8f9c-3fc4-4c9e-8228-75b61fbf6e1e" class="monProduitEstDefaillantcommentLeReparer">Mon produit <br />est défaillant.<br />Comment le réparer ?</div>
+        <div data-layer="147cfb04-91dd-4d8c-944c-f67cf88f8d42" class="ferARepasserCalor">Fer à repasser Calor</div>
+        <div data-layer="35edfc55-d4f0-4784-b8a4-64739de606dd" class="jeVeuxSavoirSiCeProduitEstFacilementReparable">Je veux savoir si ce produit est facilement réparable</div>
+        <div data-layer="2ab72abd-5cec-4ff9-a161-ce51e51c1861" class="justelanoteCopie">            <div data-layer="256be8fa-3dae-438b-9d2e-af3ed29abc7a" class="cover">                <svg data-layer="df777a81-7d48-45e4-a221-eaec3d9bd59b" preserveAspectRatio="none" viewBox="6.600012302398682 20.19999885559082 270.20001220703125 104.09999084472656" class="trace39"><path d="M 269.2000122070312 124.3000030517578 L 14.20001220703125 124.3000030517578 C 10.00001239776611 124.3000030517578 6.600012302398682 120.9000015258789 6.600012302398682 116.7000045776367 L 6.600012302398682 27.79999923706055 C 6.600012302398682 23.59999847412109 10.00001239776611 20.19999885559082 14.20001220703125 20.19999885559082 L 269.2000122070312 20.19999885559082 C 273.4000244140625 20.19999885559082 276.8000183105469 23.59999847412109 276.8000183105469 27.79999923706055 L 276.8000183105469 116.6999969482422 C 276.7999877929688 120.9000015258789 273.3999938964844 124.3000030517578 269.2000122070312 124.3000030517578 Z"  /></svg>
+                <div data-layer="bac0dae7-c3d1-4623-bef1-5f6fffd723fc" class="x4">4</div>
+                <div data-layer="c58121ad-c276-45e8-934d-0e02846edc14" class="x670fac5b">/</div>
+                <div data-layer="ac4d17b7-685f-4e6b-8f3b-cb1c59d5236a" class="x54f55bf33">5</div>
+                <svg data-layer="a33500d9-395f-4cfa-8fce-531d7eaa9e13" preserveAspectRatio="none" viewBox="162.89999389648438 74.9000015258789 11.20001220703125 15.300018310546875" class="trace40"><path d="M 166.6000061035156 74.90000152587891 L 174.1000061035156 74.90000152587891 L 168.1000061035156 90.19999694824219 L 162.8999938964844 90.19999694824219 L 166.6000061035156 74.90000152587891 Z"  /></svg>
+                <div data-layer="5b9409e9-cb23-4fe3-bc60-2faabc16ee7c" class="groupe49">                    <div data-layer="0833d8c0-01d5-46a1-8953-da5758261b46" class="x5">5</div>
+</div>
+</div>
+            <div data-layer="b420af79-d8e6-4c82-82e8-fc20d7e84795" class="rectangle25"></div>
+</div>
+        <div data-layer="68347746-56c5-4f08-a1da-136c6cf17d70" class="repairTag">REPAIR TAG</div>
+        <div data-layer="7889ec4c-9b21-4ba8-b2ca-e2f821fd489b" class="rectangle33"></div>
+        <div data-layer="82e2aae2-e792-4dbe-aee5-5a4307555a21" class="voirLesSuggestions">Voir les suggestions</div>
+        <div data-layer="254c3d65-7a0c-42e2-84a5-487248f47ee3" class="jeSouhaiteraisconnaitreLesObjetsAvecUnBonIndice">Je souhaiterais<br />connaître les objets <br />avec un bon indice</div>
+        <div data-layer="f12a7a58-5fab-4e87-907a-760679117fe4" class="rectangle31"></div>
+        <div data-layer="8829a477-bc79-489f-a49c-1a8ae19c2c36" class="rectangle32"></div>
+        <div data-layer="ee49b5fa-7f45-4cf5-8231-9fad6a96f2be" class="accesPro"><span class="accesPro-0"><<</span><span class="accesPro-2">Accès pro</span><span class="accesPro-11">>></span></div>
+</div>
+
+
+    
+    </body>
+    </html>
+            
